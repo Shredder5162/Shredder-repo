a59ce35290787c070f6d145ecd894808f25922df
Shredder5162 <shredderworld1996@gmail.com>
2026-03-27 15:22:14 +0530

    Create version.tf

---
+++ version.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+provider "github" {
+  token = var.github_token
+  owner = var.github_organization
+}
